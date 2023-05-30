1a5d57b6e4e4e6933a4da8080afc2ff9fb11bad0
philippembambi <philippembambi413@gmail.com>
2023-05-30 18:57:47 +0100

    major changes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # math-info-meta-package
 This is a meta-package that helps students in maths and computer science to easily install useful and required softwares for their academic curcus
+
+# Steps
+- git clone https://github.com/philippembambi/math-info-meta-package.git
+- cd math-info-meta-package
+- bash main.sh
--- main.sh
+++ main.sh
@@ -1,1 +1,44 @@
-echo"Hello world"
+#!bin/bash
+echo "Veillez patienter ..."
+install_dependencies(){
+    sudo apt install equivs
+}
+build_packages(){
+    equivs-build package-all
+    equivs-build package-chrome
+    equivs-build package-lamp-env
+    equivs-build package-java
+    
+    sudo apt install ./package-all_1.0_all
+    sudo apt install ./package-chrome_1.0_all
+    sudo apt install ./package-lamp-env_1.0_all
+    sudo apt install ./package-java_1.0_all
+}
+
+install_dependencies
+build_packages
+
+provided_apps=("vscode", "chrome", "framework.net", "netbeans", "codeblock", "lamp")
+echo -e ""
+echo "Salut $USER !"
+echo -e ""
+echo "*********** Bienvenue au departement de MATH-INFO *********** "
+echo -e ""
+echo "Voici la liste des programmes utiles pour un étudiant en formation"
+
+for app in "${provided_apps[@]}"
+do
+echo "$app"
+done
+
+echo -e ""
+echo "Listez vos programmes de choix séparés par des espaces [all : pour tout sélectionner]"
+echo -e ""
+read -a apps
+
+if [ "${apps[0]}" == "all" ]
+then
+    echo "Vous avez choisi de tout installer !"
+else
+    echo "Vous avez selectionné : ${apps[@]}"
+fi
